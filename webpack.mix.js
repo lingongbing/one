c06bf532ff201b8290a3a978b562bd802eb7fb54
lingongbing <lingongbing@gmail.com>
2018-01-26 23:12:53 +0800

    修改目录结构

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/homes/js/app.js', 'public/homes/js')
+    .sass('resources/assets/homes/sass/app.scss', 'public/homes/css');
 
-mix.browserSync('one.com');
+mix.browserSync('localhost');
