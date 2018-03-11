057fc0166d8449ce26b3c86f9ada876e8c66f007
lingongbing <lingongbing@gmail.com>
2018-03-11 20:48:06 +0800

    DELETE Lo-dash

--- resources/assets/admins/js/bootstrap.js
+++ resources/assets/admins/js/bootstrap.js
@@ -4,8 +4,6 @@ try {
     require('bootstrap-sass');
 } catch (e) {}
 
-import './bootstrap-datetimepicker.min';
-
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
