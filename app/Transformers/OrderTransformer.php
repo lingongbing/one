836f928ba98907f2fe33ceb5cbdb98a275c992c1
lingongbing <lingongbing@gmail.com>
2018-03-12 14:39:19 +0800

    修复商品被删除时，如果某个订单正好是被删除的商品会报错的错误

--- app/Transformers/OrderTransformer.php
+++ app/Transformers/OrderTransformer.php
@@ -13,13 +13,13 @@ class OrderTransformer extends TransformerAbstract
 			'id' => $order->id,
 			'name' => $order->user->name ? $order->user->name : $order->user->username,
 			'order_no' => $order->order_no,
-			'goods_name' => $order->good->name,
+			'goods_name' => $order->good && $order->good->name ? $order->good->name : '商品不存在',
 			'courier_order_no' => $order->courier_order_no,
 			'goods_number' => $order->goods_number,
 			'goods_total_price' => $order->goods_total_price,
 			'courier_company' => $order->courier_company,
 			'created_at' => $order->created_at->format('Y-m-d H:i:s'),
-			'goods_image' => $order->good->image,
+			'goods_image' => $order->good && $order->good->image ? $order->good->image : '',
 			'state' => $order->state
 		];
 	}
