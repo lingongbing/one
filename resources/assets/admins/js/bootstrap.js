7c69c081f32be481ccd85d4de66c0a53b4a10905
lingongbing <lingongbing@gmail.com>
2018-03-13 14:52:28 +0800

    删除没用的css引用

--- resources/assets/admins/js/bootstrap.js
+++ resources/assets/admins/js/bootstrap.js
@@ -1,9 +1,11 @@
 try {
-    window.$ = window.jQuery = require('jquery');
+	window.$ = window.jQuery = require('jquery/dist/jquery.slim');
 
     require('bootstrap-sass');
 } catch (e) {}
 
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+// import 'quill/dist/quill.core.css'
+// import 'quill/dist/quill.snow.css'
+// import 'quill/dist/quill.bubble.css'
+
+import './bootstrap-datetimepicker.min'
